6f6e7d4ae331c6fd1348c5a75480bf0a087326cc
Paullo Cesar <pc_200944@hotmail.com>
2020-12-07 06:29:17 -0300

    Functional Components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,69 +1,71 @@
-import React, { Component } from 'react'
+import React, {useState,useEffect} from 'react'
 import Header from './components/Header';
 import Spinner from './components/Spinner';
 import Candidates from './components/Candidates';
-export default class App extends Component {
-  constructor() {
-    super();
 
-    this.state={
-      candidates: [],
-      previousVotes: [],
-      previousPercentages: [],
-    };
+export default function App () {
 
-    this.interval = null;
+  const [candidates, setCandidates] = useState([])
+  const [previousVotes, setPreviousVotes] = useState([])
+  const [previousPercentages, setPreviousPercentages] = useState([])
 
-  }
-  componentDidMount() {
-    this.interval = setInterval(() =>{
 
-      fetch('http://localhost:8080/votes')
-      .then((res) => {
+  useEffect(() => {
+    const interval = setInterval(() =>{
 
-        return res.json();
-        
-        })
-        .then((json)=>{
-
-          const previousVotes = this.state.candidates.map(({id, votes}) =>{
-              return {id, votes};
-          });
-
-          const previousPercentages = this.state.candidates.map(({id, percentage}) =>{
-            return {id, percentage};
-         });
-
-         this.setState({
-           candidates: json.candidates,
-           previousVotes,
-           previousPercentages,
-         })
-        });
-      
-    },1000  )
-  }  
-  
-  render() {
-      const {candidates , previousVotes, previousPercentages} = this.state;
-
-      if(candidates.length === 0 ){
+          fetch('http://localhost:8080/votes')
+          .then((res) => {
     
-        return <Spinner description="Carregando..."/>
-      }
+            return res.json();
+            
+            })
+            .then((json)=>{
+    
+              const localPreviousVotes = candidates.map(({id, votes}) =>{
+                  return {id, votes};
+              });
+    
+              const localPreviousPercentages = candidates.map(({id, percentage}) =>{
+                return {id, percentage};
+             });
 
-      return (
-        <div className="container">
 
-          <Header>Votação</Header>
-          <Candidates 
-            previousPercentages = {previousPercentages}
-            previousVotes={previousVotes} 
-            candidates={candidates}
-         />
+             setCandidates(json.candidates);
+             setPreviousVotes(localPreviousVotes);
+             setPreviousPercentages(localPreviousPercentages);
+    
+            //  setState({
+            //    candidates: json.candidates,
+            //    previousVotes,
+            //    previousPercentages,
+            //  })
+
+
+            });
           
-          
-        </div>
-        );
-  }
+        },1000  )
+    return () => {
+      clearInterval(interval);
+    }
+  }, [candidates])
+  
+
+    if(candidates.length === 0 ){
+  
+      return <Spinner description="Carregando..."/>
+    }
+
+    return (
+      <div className="container">
+
+        <Header>Votação</Header>
+        <Candidates 
+          previousPercentages = {previousPercentages}
+          previousVotes={previousVotes} 
+          candidates={candidates}
+        />
+        
+        
+      </div>
+      );
 }
